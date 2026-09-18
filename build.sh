7592a25335166eaa1b47a823eb80645c7f092338
Sai Ganesh <your-email@gmail.com>
2026-09-18 10:52:53 +0530

    Create Samuel Render superuser

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 set -o errexit
 
 pip install -r requirements.txt
@@ -6,3 +7,34 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+# Create/update Django superuser
+if [ -n "${DJANGO_SUPERUSER_USERNAME:-}" ] && \
+   [ -n "${DJANGO_SUPERUSER_EMAIL:-}" ] && \
+   [ -n "${DJANGO_SUPERUSER_PASSWORD:-}" ]; then
+
+    python manage.py shell -c "
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.environ['DJANGO_SUPERUSER_USERNAME']
+email = os.environ['DJANGO_SUPERUSER_EMAIL']
+password = os.environ['DJANGO_SUPERUSER_PASSWORD']
+
+user, created = User.objects.get_or_create(
+    username=username,
+    defaults={'email': email}
+)
+
+user.email = email
+user.is_staff = True
+user.is_superuser = True
+user.is_active = True
+user.set_password(password)
+user.save()
+
+print('Superuser created/updated:', username)
+"
+fi
